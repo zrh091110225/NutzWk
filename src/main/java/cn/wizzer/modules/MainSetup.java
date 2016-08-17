e5da4c505d612ea1c00a5dd44a1bc255807d721d
ronghai.zheng <ronghai.zheng@qunar.com>
2016-08-17 15:25:08 +0800

    去除数据库初始化操作

--- src/main/java/cn/wizzer/modules/MainSetup.java
+++ src/main/java/cn/wizzer/modules/MainSetup.java
@@ -1,11 +1,9 @@
 package cn.wizzer.modules;
 
 import cn.wizzer.common.mvc.config.Dict;
-import cn.wizzer.common.service.RedisService;
 import cn.wizzer.common.util.CacheUtils;
 import cn.wizzer.modules.sys.bean.*;
 import cn.wizzer.common.mvc.config.Globals;
-import cn.wizzer.modules.sys.service.ConfigService;
 import cn.wizzer.modules.sys.service.DictService;
 import net.sf.ehcache.CacheManager;
 import org.apache.shiro.crypto.RandomNumberGenerator;
@@ -27,8 +25,6 @@ import org.nutz.mvc.Mvcs;
 import org.nutz.mvc.NutConfig;
 import org.nutz.mvc.Setup;
 import org.nutz.integration.quartz.NutQuartzCronJobFactory;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 
 import java.io.IOException;
 import java.util.*;
@@ -39,13 +35,12 @@ import java.util.*;
 public class MainSetup implements Setup {
     private static final Log log = Logs.get();
     static DictService dictService = Mvcs.ctx().getDefaultIoc().get(DictService.class);
-    static ConfigService configService = Mvcs.ctx().getDefaultIoc().get(ConfigService.class);
     public void init(NutConfig config) {
         try {
             Ioc ioc = config.getIoc();
             Dao dao = ioc.get(Dao.class);
-            // 初始化数据表 初始化一次即可
-            initSysData(config, dao);
+            // 初始化数据表
+            //initSysData(config, dao);//TODO 需要自动建表和插入初始化数据是打开
 
             // 初始化Velocity
             velocityInit(config);
@@ -56,19 +51,7 @@ public class MainSetup implements Setup {
             // 检查一下Ehcache CacheManager 是否正常.
             CacheManager cacheManager = ioc.get(CacheManager.class);
             log.debug("Ehcache CacheManager = " + cacheManager);
-            /* redis测试
-            JedisPool jedisPool = ioc.get(JedisPool.class);
-            try (Jedis jedis = jedisPool.getResource()) {
-                String re = jedis.set("_big_fish", "Hello Word!!");
-                log.debug("1.redis say : " + re);
-                re = jedis.get("_big_fish");
-                log.debug("2.redis say : " + re);
-            } finally {}
 
-            RedisService redis = ioc.get(RedisService.class);
-            redis.set("hi", "wendal,rekoe hoho..");
-            log.debug("redis say again : " + redis.get("hi"));
-            */
             // 初始化系统变量
             initSysSetting(config, dao);
         } catch (Exception e) {
